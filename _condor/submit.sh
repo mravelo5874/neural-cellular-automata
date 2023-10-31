49494d199dc1e991e9f5c1e894ceee5323aba91a
Marco Ravelo <mravelo5874@gmail.com>
2023-10-31 17:27:24 -0500

    added venv

--- _condor/submit.sh
+++ _condor/submit.sh
@@ -1,7 +1,5 @@
 #!/lusr/bin/bash
 source ~/.bashrc
-# eval "$('/u/jmz679/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-# conda activate /scratch/cluster/jmz679/envs/automatic
-# accelerate launch videonet_training.py
-cd /u/marco/github/neural-cellular-automata/5_voxel_nca
-python3 train.py
+cd /u/marco/github/neural-cellular-automata
+source venv/bin/activate
+python3 /5_voxel_nca/train.py
