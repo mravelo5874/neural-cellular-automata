2f55fa0ec031dd3193f541e7408a1a7c2a0b2cf6
Marco Ravelo <mravelo5874@gmail.com>
2023-11-01 10:31:46 -0500

    Create create_venv.sh

---
+++ _condor/create_venv.sh
@@ -0,0 +1,10 @@
+#!/lusr/bin/bash
+source ~/.bashrc
+cd /u/marco/github/neural-cellular-automata
+python3 -m venv venv
+source venv/bin/activate
+pip install numpy
+pip install torch
+pip install matplotlib
+pip install requests
+pop install moviepy
