30f9e861531275472a9a99cec2cd2244abd7edab
Marco Ravelo <mravelo5874@gmail.com>
2023-11-02 00:07:12 -0500

    Update train.py

--- 5_voxel_nca/train.py
+++ 5_voxel_nca/train.py
@@ -187,7 +187,7 @@ def main():
                 iter_per_sec = float(i)/float(secs)
                 est_time_sec = int((_EPOCHS_-i)*(1/iter_per_sec))
                 est = str(datetime.timedelta(seconds=est_time_sec))
-                avg = loss_log[-_INFO_RATE_:]/_INFO_RATE_
+                avg = sum(loss_log[-_INFO_RATE_:])/float(_INFO_RATE_)
                 lr = np.round(lr_sched.get_last_lr()[0], 8)
                 step = '▲'
                 if prev_lr > lr:
