814212b58be769d294ea604925edbd6181de6e78
Marco Ravelo <mravelo5874@gmail.com>
2023-11-02 06:42:39 -0500

    Update vidgen.py

--- 5_voxel_nca/vidgen.py
+++ 5_voxel_nca/vidgen.py
@@ -11,7 +11,8 @@ def main():
     start = datetime.datetime.now()
     
     # * vidgen params
-    _MODEL_ = '_models/cowboy16_aniso'
+    _NAME_ = 'earth_aniso'
+    _MODEL_ = '_models/earth_aniso'
     _VIDEO_ = '_videos/cowboy16_test'
     _DEVICE_ = 'cuda'
     
@@ -37,7 +38,9 @@ def main():
     # * generate video
     print ('generating video...')
     with torch.no_grad():
-        model.generate_video(f'{_VIDEO_}.mp4', seed_ten)
+        #model.generate_video(f'{_VIDEO_}.mp4', seed_ten)
+        model.regen_video(f'_videos/{_NAME_}_multi_regen.mp4', seed_ten, _size=_SIZE_+(2*_PAD_), _mask_types=['x+', 'y+', 'z+'])
+        model.rotate_video(f'_videos/{_NAME_}_multi_rotate.mp4', seed_ten, _size=_SIZE_+(2*_PAD_))
         
      # * calculate elapsed time
     secs = (datetime.datetime.now()-start).seconds
