a743aa350242721224ed2c78088209b77164df2b
Marco Ravelo <mravelo5874@gmail.com>
2023-11-13 10:56:35 -0600

    fix lr for yaw iso 9

---
+++ 6_voxel_engine/_objects/voxobj.py
@@ -0,0 +1,28 @@
+from settings import *
+from _meshes.chunk_mesh import ChunkMesh
+
+class VoxObj:
+    def __init__(self, _app, _vox):
+        self.app = _app
+        self.voxels: np.array = self.build_voxels()
+        self.mesh: ChunkMesh = None
+        self.build_mesh()
+        
+    def build_mesh(self):
+        self.mesh = ChunkMesh(self)
+        
+    def render(self):
+        self.mesh.render()
+    
+    def build_voxels(self):
+        # * create empty chunk
+        voxels = np.zeros(CHUNK_VOL, dtype='uint8')
+        
+        # * fill chunk
+        for x in range(CHUNK_SIZE):
+            for y in range(CHUNK_SIZE):
+                for z in range(CHUNK_SIZE):
+                    voxels[x+CHUNK_SIZE*z+CHUNK_AREA*y] = (
+                        x+y+z if int(glm.simplex(glm.vec3(x, y, z)*0.1)+1) else 0
+                    )
+        return voxels
