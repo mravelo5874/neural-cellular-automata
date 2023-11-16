5aa8c505b33d75f6ba4bbd8f9472a21422b7d94e
Marco Ravelo <mravelo5874@gmail.com>
2023-11-15 22:06:48 -0600

    Update train3d.py

--- _5_voxel_nca/train3d.py
+++ _5_voxel_nca/train3d.py
@@ -13,7 +13,7 @@ from scripts.nca import VoxelUtil as util
 from scripts.vox.Vox import Vox
 
 # * target/seed parameters
-_NAME_ = 'bally4_yawiso_0'
+_NAME_ = 'earth_yawiso_0'
 _SIZE_ = 4
 _PAD_ = 4
 _SEED_DIST_ = 2
@@ -26,12 +26,12 @@ _SEED_DIC_ = {
     'plus_z': 'blue',
     'minus_z': None,
 }
-_TARGET_VOX_ = '../_vox/bally4.vox'
+_TARGET_VOX_ = '../vox/earth.vox'
 # * model parameters
 _MODEL_TYPE_ = Perception.YAW_ISO
 _CHANNELS_ = 16
 # * training parameters
-_EPOCHS_ = 5_000
+_EPOCHS_ = 20_000
 _BATCH_SIZE_ = 4
 _POOL_SIZE_ = 32
 _UPPER_LR_ = 1e-3
