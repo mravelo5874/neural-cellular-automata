3c3d0b078f5c10fa8643cb0c6eb13909227d6a22
Marco Ravelo <mravelo5874@gmail.com>
2024-03-05 13:35:49 -0600

    Update train3d.py

--- _5_voxel_nca/train3d.py
+++ _5_voxel_nca/train3d.py
@@ -213,11 +213,11 @@ def main():
     # * save model isotropic type
     ISO_TYPE = model.isotropic_type()    
         
-    # * use multiple gpus
-    if _DEVICE_ == 'cuda':
-        voxutil.logprint(f'_models/{_NAME_}/{_LOG_FILE_}', f'setting model to use multiple GPUs (if available)...')
-        devices = [0, 1, 2, 3]
-        model = torch.nn.DataParallel(model, device_ids=devices)
+    # * use multiple GPUs
+    # if _DEVICE_ == 'cuda':
+    #     voxutil.logprint(f'_models/{_NAME_}/{_LOG_FILE_}', f'setting model to use multiple GPUs (if available)...')
+    #     devices = [0, 1, 2, 3]
+    #     model = torch.nn.DataParallel(model, device_ids=devices)
     
     # * create optimizer and learning-rate scheduler
     opt = torch.optim.Adam(model.parameters(), _UPPER_LR_)
