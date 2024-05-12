fccbe9c6070557bade8669fdd887f5d7bc9d072c
Marco Ravelo <mravelo5874@gmail.com>
2024-05-12 16:27:06 -0500

    added condor .sh file

---
+++ condor/trainDDP/trainDDP.sh
@@ -0,0 +1,6 @@
+#!/lusr/bin/bash
+source ~/.bashrc
+cd /u/marco/github/neural-cellular-automata
+source venv/bin/activate
+cd /u/marco/github/neural-cellular-automata/_9_post_thesis_optimizations
+python3 train.py
