434eb1f65b6bb2930a802b5d5cd5adaa09def9b7
Marco Ravelo <mravelo5874@gmail.com>
2024-05-15 22:00:50 -0500

    remade create_venv.sh

--- condor/create_venv.sh
+++ condor/create_venv.sh
@@ -3,11 +3,4 @@ source ~/.bashrc
 cd /u/marco/github/neural-cellular-automata
 python3 -m venv venv
 source venv/bin/activate
-pip install --no-cache-dir numpy 
-pip install --no-cache-dir torch torchvision torchaudio
-pip install --no-cache-dir matplotlib
-pip install --no-cache-dir requests
-pip install --no-cache-dir moviepy
-pip install --no-cache-dir scipy
-pip install --no-cache-dir gitpython
-pip install --no-cache-dir "git+https://github.com/facebookresearch/pytorch3d.git@stable"
+pip install --no-cache-dir -r condor/requirements.txt
