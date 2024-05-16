0c2a87af39e0ea788e153d69215b58374af55366
Marco Ravelo <mravelo5874@gmail.com>
2024-05-15 22:20:02 -0500

    Update create_venv.sh

--- condor/create_venv.sh
+++ condor/create_venv.sh
@@ -1,7 +1,9 @@
 #!/lusr/bin/bash
+echo "creating venv for training nca..."
 source ~/.bashrc
 cd /u/marco/github/neural-cellular-automata
 python3 -m venv venv
 source venv/bin/activate
 pip install --no-cache-dir -r condor/requirements.txt
 pip install --no-cache-dir "git+https://github.com/facebookresearch/pytorch3d.git@stable"
+echo "finished creating venv..."
